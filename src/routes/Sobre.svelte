a04fceb3c8562657ea839dee28610dee5148a139
Jorge Sarkis <86751142+JorgeSaicoski@users.noreply.github.com>
2023-02-01 13:03:59 -0300

    Sobre init

--- src/routes/Sobre.svelte
+++ src/routes/Sobre.svelte
@@ -1,6 +1,9 @@
 <section id="sobre">
-    <h1>Sarkis.Dev</h1>
-    <h3>Tenga su aplicacion desarrolada por una equipe especializadad en empreendedorismo.</h3>
+    <div>
+        <h1>Sarkis.Dev</h1>
+        <h3>Tenga su aplicacion desarrolada por una equipe especializadad en empreendedorismo.</h3>
+        <p>Como el tero, el centinela del pampa, estamos siempre atento a las necesidades de nuestros clientes y a las nuevas tecnologias.</p>
+    </div>
     <div class="mision">
         <h3>Misión</h3>
         <p>Desarollar aplicaciones para ayudar empresas y empleados a alcanzaren sus objetivos.</p>
@@ -10,12 +13,6 @@
         <p>Tener una base de clientes que permita alquilar los mejores servidores.</p>
     </div>
     <div class="valores">
-        <!--
-        <div class="texto">
-            <h3>Respeto al plazo</h3>
-            <p>Sabremos cuan importante es tener su negocio abierto y funcionando en dia. El tiempo es una de las cosas mas cara para un empreendimento y lo respetamos.</p>
-        </div>
-        -->
         <div class="texto">
             <h3>Flexibilidad</h3>
             <p>Tenemos nuestros servicios base. Pero, siempre estamos abierto en realizar un nuevo servicio que pueda ayudar nuestro cliente a algun objetivo especifico.</p>
@@ -29,8 +26,7 @@
         <div class="texto">
             <h3>Liberdad del cliente</h3>
             <p>Entiendemos que las relaciones pueden tener fin. Y una relacion comercial no es excepción. Por eso nuestros proyectos siempre estan preparados para que el cliente siga sin nosostros</p>
-            <p>Los codigos son "abiertos" y puedes contactar otro profesional o empresa para despegar la aplicación.</p>
-            <p>Nuestro diferencial es la flexibilidad y atencion. Si el cliente no esta satisfecho con esto, podrá buscar nuevas soluciones.</p>
+            <p>Los codigos son "abiertos" y puedes contactar otro profesional o empresa para despegar la aplicación. Como el tero, nuestro codigo es libre.</p>
         </div>
     </div>
 </section>
