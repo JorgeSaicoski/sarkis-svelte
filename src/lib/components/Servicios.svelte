d5220851ae1bcef071d3789a62bba2161e74ddd2
Jorge Sarkis <86751142+JorgeSaicoski@users.noreply.github.com>
2023-02-10 18:35:00 -0300

    css improve

--- src/lib/components/Servicios.svelte
+++ src/lib/components/Servicios.svelte
@@ -3,29 +3,31 @@
 </script>
 
 <h2>Servicios</h2>
-<div>
-    <h3>Creación de Portfolio Web</h3>
-    <p>Eres profesional y necesitas mostrar tu trabajo?</p>
-    <p>Te ayudamos a crear un portfolio online. Con el podrás crear un portfolio que sea facil de mantener actualizado.</p>
-    <p>Por padron, hacemos una pagina con una lista de proyectos/productos categorizadas de la manera que el cliente elija, con paginas de detalles y un sector para contactar.</p>
+<div class="container">
+    <div>
+        <h3>Creación de Portfolio Web</h3>
+        <p>Eres profesional y necesitas mostrar tu trabajo?</p>
+        <p>Te ayudamos a crear un portfolio online. Con el podrás crear un portfolio que sea facil de mantener actualizado.</p>
+    </div>
+    <div>
+        <h3>Aplicaciones Web y Mobile</h3>
+        <p>Necesitas de una aplicacion para tu negocio?</p>
+        <p>Controle de estoque, formularios de controle, plataforma de clases...</p>
+        <p>Te ayudamos a planear, crear y implementar la aplicacion.</p>
+    </div>
+    <div>
+        <h3>Despliegue de Aplicaciones</h3>
+        <p></p>
+    </div>
+    <div>
+        <h3>Desarrollo de Empreendedorismo</h3>
+        <p></p>
+    </div>
+    <div>
+        <h3>eCommerce</h3>
+        <p></p>
+    </div>
 </div>
-<div>
-    <h3>Aplicaciones Web y Mobile</h3>
-    <p>Necesitas de una aplicacion para tu negocio?</p>
-    <p>Controle de estoque, formularios de controle, plataforma de clases...</p>
-    <p>Te ayudamos a planear, crear y implementar la aplicacion.</p>
-</div>
-<div>
-    <h3>Despliegue de Aplicaciones</h3>
-    <p></p>
-</div>
-<div>
-    <h3>Desarrollo de Empreendedorismo</h3>
-    <p></p>
-</div>
-<div>
-    <h3>eCommerce</h3>
-    <p></p>
-</div>
+
 
 
