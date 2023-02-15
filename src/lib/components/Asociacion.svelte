48e9ccd34853167524030d6cd79018e62047bdd0
Jorge Sarkis <86751142+JorgeSaicoski@users.noreply.github.com>
2023-02-15 15:02:25 -0300

    cards asociacion

--- src/lib/components/Asociacion.svelte
+++ src/lib/components/Asociacion.svelte
@@ -1,15 +1,44 @@
-<h2>Asociacion</h2>
-<div>
-    <div class="card">
-        <h2>Wozcode</h2>
+<script>
+    import DarkMode from "$lib/store/dark.js";
+    import wozcode from "$lib/images/wozcode.png";
+
+    let dark
+    DarkMode.subscribe((mode)=>{
+        dark = mode
+    })
+</script>
+<h2 class="baseText darkDraw"
+        class:base={dark==="base"}
+        class:white={dark==="dark"}
+>Asociacion</h2>
+<div class="container">
+    <div class="card"
+         class:base={dark==="base"}
+         class:dark={dark==="dark"}
+         class:baseText={dark==="dark"}
+         class:whiteText={dark==="base"}
+    >
+        <h3
+            class:dark={dark==="base"}
+            class:base={dark==="dark"}
+        >Wozcode</h3>
+        <img src="{wozcode}">
         <div class="description">
             <p>Wozcode es una empresa de creacion de SaaS a medida.</p>
             <p>Compartimos desarrolladores y experiencias con esta gran empresa.</p>
         </div>
         <a href="https://wozcode.com/">Visitar</a>
     </div>
-    <div class="card">
-        <h2>Dev Dropper</h2>
+    <div class="card"
+         class:base={dark==="base"}
+         class:dark={dark==="dark"}
+         class:baseText={dark==="dark"}
+         class:whiteText={dark==="base"}
+    >
+        <h3
+            class:dark={dark==="base"}
+            class:base={dark==="dark"}
+        >Dev Dropper</h3>
         <div class="description">
             <p>Dev Dropper es una empresa de creacion de pequeños codigos.</p>
             <p>Así pueden crear complejos softwares rapidamente y con calidad, ya que estan pre-armados.</p>
@@ -17,8 +46,16 @@
         </div>
         <a href="https://devdropper.com/">Visitar</a>
     </div>
-    <div class="card">
-        <h2>Jorge Adriano</h2>
+    <div class="card"
+         class:base={dark==="base"}
+         class:dark={dark==="dark"}
+         class:baseText={dark==="dark"}
+         class:whiteText={dark==="base"}
+    >
+        <h3
+            class:dark={dark==="base"}
+            class:base={dark==="dark"}
+        >Jorge Adriano</h3>
         <div class="description">
             <p>Jorge Adriano es el creador de Sarkis Dev.</p>
             <p>Graduado en administracción, fue tutor de materias de finanzas, hoy trabaja con desarrollo web.</p>
