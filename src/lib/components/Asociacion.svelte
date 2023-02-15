62eea56a8f9c652ea20f5d6e6af6cddd8112db0e
Jorge Sarkis <86751142+JorgeSaicoski@users.noreply.github.com>
2023-02-15 13:09:47 -0300

    spanish fixed

--- src/lib/components/Asociacion.svelte
+++ src/lib/components/Asociacion.svelte
@@ -12,9 +12,18 @@
         <h2>Dev Dropper</h2>
         <div class="description">
             <p>Dev Dropper es una empresa de creacion de pequeños codigos.</p>
-            <p>Así pueden crear complejos softwares rapidamente y con calidad, ya que esta (pre armado).</p>
+            <p>Así pueden crear complejos softwares rapidamente y con calidad, ya que estan pre-armados.</p>
             <p>Desarrollamos nuestros softwares en conjunto.</p>
         </div>
-        <a href="https://wozcode.com/">Visitar</a>
+        <a href="https://devdropper.com/">Visitar</a>
+    </div>
+    <div class="card">
+        <h2>Jorge Adriano</h2>
+        <div class="description">
+            <p>Jorge Adriano es el creador de Sarkis Dev.</p>
+            <p>Graduado en administracción, fue tutor de materias de finanzas, hoy trabaja con desarrollo web.</p>
+            <p>Todavia utiliza sus conocimientos de administraccion para gestionar Sarkis Dev.</p>
+        </div>
+        <a href="https://jorgeadriano.com/">Visitar</a>
     </div>
 </div>
