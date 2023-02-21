0453764f9a396a1388948f751d2a44ebbf5a1d12
Jorge Sarkis <86751142+JorgeSaicoski@users.noreply.github.com>
2023-02-21 15:41:05 -0300

    traffic improved

--- src/lib/components/presupuesto/Traffic.svelte
+++ src/lib/components/presupuesto/Traffic.svelte
@@ -18,15 +18,15 @@
 <form on:submit={submitNext}>
     <label>
         <input type="radio" bind:group={customerTraffic} value="small">
-        Menos de 1000 por mes
+        Menos de 5000 por mes
     </label>
     <label>
         <input type="radio" bind:group={customerTraffic} value="middle">
-        Mas de 1000 y menos de 5000 acesos por mes
+        Mas de 5000 y menos de 10000 acesos por mes
     </label>
     <label>
         <input type="radio" bind:group={customerTraffic} value="big">
-        Mas 5000 acesos por mes
+        Mas 10000 acesos por mes
     </label>
     <button type="submit">Proximo</button>
 </form>
